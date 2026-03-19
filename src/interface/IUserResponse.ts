84ecdecce4aa135992eee010406a4e9df4e41679
Supernatural123qwert1q2w3e4r5te <gunaevart@mail.ru>
2026-03-19 14:32:49 +0300

    lessons ts express

---
+++ src/interface/IUserResponse.ts
@@ -0,0 +1,8 @@
+interface UserResponse{
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
+export default UserResponse;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express";
-const app = express()
-
+import UserResponse from "./interface/IUserResponse";
 import dotenv from "dotenv";
+
+const app = express()
+app.use(express.json());
 dotenv.config()
 const PORT = process.env.PORT;
 
 
+const userData: UserResponse[] = []
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -13,6 +16,46 @@ app.get("/", (req: Request, res: Response) => {
     message: "hello world",
   });
 });
+
+app.get("/api/users", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    count: userData.length,
+    data: userData
+  })
+})
+
+
+app.get("/api/user/:id", (req: Request, res: Response<UserResponse>) => {
+  
+  const user: UserResponse = {
+    id: 1,
+    name: "John Doe ",
+    email: "john@example.ru",
+    password: "123456"
+    
+  }
+
+  res.json(user)
+
+});
+
+
+app.post("/api/users", (req: Request, res: Response) => {
+  const id_rand = Math.floor(Math.random() * 10);
+  const { name, email, password } = req.body;
+  const users: UserResponse = {
+    id: id_rand,
+    name: name,
+    email: email,
+    password: password
+  }
+  
+  userData.push(users)
+  res.status(201).json(userData)
+  console.log(userData)
+});
+
 
 
 
